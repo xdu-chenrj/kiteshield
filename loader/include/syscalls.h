5e035c187c31e6087077d7d2011445bec73b752b
xdu-chenrj <chenrj.xdu@gmail.com>
2022-10-26 14:22:25 +0800

    update syscalls.h

--- loader/include/syscalls.h
+++ loader/include/syscalls.h
@@ -292,21 +292,23 @@ enum rlimit_resource {
  * These bits should be set in the mask argument of statx() to request
  * particular items when calling statx().
  */
-#define STATX_TYPE		0x00000001U	/* Want/got stx_mode & S_IFMT */
-#define STATX_MODE		0x00000002U	/* Want/got stx_mode & ~S_IFMT */
-#define STATX_NLINK		0x00000004U	/* Want/got stx_nlink */
-#define STATX_UID		0x00000008U	/* Want/got stx_uid */
-#define STATX_GID		0x00000010U	/* Want/got stx_gid */
-#define STATX_ATIME		0x00000020U	/* Want/got stx_atime */
-#define STATX_MTIME		0x00000040U	/* Want/got stx_mtime */
-#define STATX_CTIME		0x00000080U	/* Want/got stx_ctime */
-#define STATX_INO		0x00000100U	/* Want/got stx_ino */
-#define STATX_SIZE		0x00000200U	/* Want/got stx_size */
-#define STATX_BLOCKS		0x00000400U	/* Want/got stx_blocks */
-#define STATX_BASIC_STATS	0x000007ffU	/* The stuff in the normal stat struct */
-#define STATX_BTIME		0x00000800U	/* Want/got stx_btime */
-#define STATX_ALL		0x00000fffU	/* All currently supported flags */
-#define STATX__RESERVED		0x80000000U	/* Reserved for future struct statx expansion */
+#define STATX_TYPE 0x00000001U        /* Want/got stx_mode & S_IFMT */
+#define STATX_MODE 0x00000002U        /* Want/got stx_mode & ~S_IFMT */
+#define STATX_NLINK 0x00000004U       /* Want/got stx_nlink */
+#define STATX_UID 0x00000008U         /* Want/got stx_uid */
+#define STATX_GID 0x00000010U         /* Want/got stx_gid */
+#define STATX_ATIME 0x00000020U       /* Want/got stx_atime */
+#define STATX_MTIME 0x00000040U       /* Want/got stx_mtime */
+#define STATX_CTIME 0x00000080U       /* Want/got stx_ctime */
+#define STATX_INO 0x00000100U         /* Want/got stx_ino */
+#define STATX_SIZE 0x00000200U        /* Want/got stx_size */
+#define STATX_BLOCKS 0x00000400U      /* Want/got stx_blocks */
+#define STATX_BASIC_STATS 0x000007ffU /* The stuff in the normal stat struct   \
+                                       */
+#define STATX_BTIME 0x00000800U       /* Want/got stx_btime */
+#define STATX_ALL 0x00000fffU         /* All currently supported flags */
+#define STATX__RESERVED                                                        \
+  0x80000000U /* Reserved for future struct statx expansion */
 
 /* syscall wrapper prototypes */
 ssize_t sys_write(int fd, const char *s, size_t count);
@@ -315,9 +317,8 @@ ssize_t sys_read(int fd, void *buf, size_t count);
 
 off_t sys_lseek(int fd, off_t offset, int whence);
 
-//int sys_open(const char *pathname, int flags, int mode);
-int sys_open(int dirfd, const char *pathname, int flags, int mode)
-
+// int sys_open(const char *pathname, int flags, int mode);
+int sys_open(int dirfd, const char *pathname, int flags, int mode);
 
 int sys_close(int fd);
 
