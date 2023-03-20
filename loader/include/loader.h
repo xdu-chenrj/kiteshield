76bf70133d51b900a2d0df41c7629fa200552340
xdu-chenrj <chenrj.xdu@gmail.com>
2023-03-20 17:28:48 +0800

    arm_v3_with_randk

--- loader/include/loader.h
+++ loader/include/loader.h
@@ -7,5 +7,21 @@
 
 #endif //KITESHIELD_LOADER_H
 
+extern struct rc4_key obfuscated_key;
+extern unsigned char serial_key[16];
 
-extern struct rc4_key obfuscated_key __attribute__((section(".key")));
+typedef struct termios termios_t;
+typedef struct serial_data {
+    unsigned char data_buf[39];
+    int ser_fd;
+} ser_data;
+
+void reverse_shuffle(unsigned char *arr, int n, unsigned char swap_infos[]);
+
+void send(ser_data snd);
+void receive(ser_data rec);
+
+int common(uint8_t serial_send[SERIAL_SIZE]);
+
+unsigned short int CRC16_Check(const unsigned char *data, unsigned char len);
+
